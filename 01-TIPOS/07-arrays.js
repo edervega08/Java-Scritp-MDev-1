483f202553afa4b6feacfcd456a88f0050974873
edervega08 <edervb2000@gmail.com>
2025-05-21 12:33:24 -0600

    actualizada

---
+++ 01-TIPOS/07-arrays.js
@@ -0,0 +1,16 @@
+let  animales=['Perro','Gato','Caballo'];
+
+console.log(animales);
+console.log(animales[0]);
+animales[2]='vaca';
+console.log(animales[2]);
+
+
+//indice que no haga sentido
+
+animales[10]='toro';
+console.log(animales[7]);
+console.log(typeof animales);//por que objeto y no array o biene por defedcto en la clse array?
+//ojetos y prototipos
+animales.length//dovolver la cantidad de elementos
+
