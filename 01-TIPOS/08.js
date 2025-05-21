ebf2cb4b01b4604f1cb419de0d52e94e2febf353
edervega08 <edervb2000@gmail.com>
2025-05-21 12:23:23 -0600

    actualizada

---
+++ 01-TIPOS/08.js
@@ -0,0 +1,23 @@
+
+//se crea una variable haciendo referencia por asi decir que despues vamos a mandar a llamar
+function saludar(){
+
+    console.log('hola mundo');
+    
+}
+saludar();
+
+function suma(){
+
+
+    return 2+2;
+
+}
+//let resultado=suma();
+//console.log(resultado);
+//o
+
+console.log(suma());//si no se pone los parentesis de la funcion que pasa?
+
+
+
---
+++ 01-TIPOS/09.js
@@ -0,0 +1,65 @@
+/*
+//argumentos y parametros
+//a la funcion se le pueden pasar parametos y usarlos dentro
+//argumentos
+function suma(a){
+
+
+    return a+2;
+}
+//esplica la estructura
+//parametos
+let resultado=suma(5);
+console.log(resultado);
+
+
+
+//ejemplo 2
+function suma2(c,d){
+
+
+    return c+d;
+}
+//esplica la estructura
+//parametos
+let resultado2=suma2(5,6);
+console.log(resultado2);
+*/
+//ejemplo 3
+
+function suma3(e,f){
+console.log(arguments);//acceder a todos los valores pero no es lo mejor
+
+    return e+f;
+}
+//esplica la estructura
+//parametos
+let resultado3=suma3(5,6,1,2,3);
+console.log(resultado3);
+console.log(typeof suma);
+
+
+//declaraciones y expresiones 
+//
+//declaration=declaracion
+//en js es algo que se va a referenciar en un futuro
+//-let
+//-const
+//var no
+//funcion *
+//asycn function*
+//class
+//export/import
+//cuales demas?
+//statemen=declaracion
+//if
+//for
+//else
+//switch
+
+//que es un expresion en js
+//cualquier linea o porcion que evalue en un valor
+//4+6=10
+//x=4
+//miFuncion()
+
